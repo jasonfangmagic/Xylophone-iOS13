1109acb22f523663a8426cfa5acad76e5110cb5a
jasonfangmagic <jasonfangmagic@gmail.com>
2024-03-04 12:14:07 -0500

    complete project

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -9,3 +9,29 @@ func greeting() {
 // var inside func can be only used inside function
 
 greeting()
+
+
+//Create a Function with Input
+
+func myFunction (/*parameter : DataType*/) {
+    /*Do sth with input*/
+}
+
+
+var myAge: String = "Three"
+
+
+//Create the function
+
+func greeting2(whoToGreet:String){
+     print("Hello \(whoToGreet)")
+}
+
+greeting2(whoToGreet: "Jaosn")
+
+
+func getMilk (bottles: Float) {
+    var cost = bottles * 1.5
+}
+
+getMilk(bottles: 4)
